3ac34c54b4c1fdafc7349f078c9bd6f06906bae0
sunepa01 <117924550+sunepa01@users.noreply.github.com>
2023-09-05 00:04:17 +0100

    Your commit message here

---
+++ src/main/java/com/example/jumia/data/models/MaximumNumber.java
@@ -0,0 +1,29 @@
+package com.example.jumia.data.models;
+
+import java.util.Scanner;
+
+public class MaximumNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of numbers you will enter");
+        int range = scanner.nextInt();
+
+
+        int count = 1;
+        System.out.println("Enter first Number");
+        int maxInput = scanner.nextInt();
+
+
+        for (int i = count; i <=  range; i++) {
+            System.out.println("enter number "+ i);
+            int userInput = scanner.nextInt();
+
+            if (userInput > maxInput){
+                maxInput = userInput;
+            }
+        }
+
+        System.out.println("max number is "+maxInput);
+    }
+}
